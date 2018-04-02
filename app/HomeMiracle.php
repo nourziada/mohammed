073f9cb3e02ee618ef119a3c128989e08775df7a
nourziada <eng.nour.ziadaa@gmail.com>
2018-04-02 10:30:55 +0300

    finish the home page control panel

---
+++ app/HomeMiracle.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class HomeMiracle extends Model
+{
+    
+}
---
+++ app/SectionBelowHeader.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class SectionBelowHeader extends Model
+{
+    protected $table = "section_below_header";
+}
